3c6b6364f2e683c82387dd213662b80ef21b6fe5
jagannathtivarekar <jagannathtivarekar@gmail.com>
2024-10-24 13:13:14 +0530

    ok

---
+++ 02_2Feb2024/Task_01.py
@@ -0,0 +1,15 @@
+
+# Take a user input (name, Age, Roll_No, Phone_Number) and print the details
+
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+roll_number = input("Enter your roll number: ")
+phone_number = input("Enter your phone number: ")
+
+# Print the details
+print("\nUser Details:")
+print("Name:", name)
+print("Age:", age)
+print("Roll Number:", roll_number)
+print("Phone Number:", phone_number)
